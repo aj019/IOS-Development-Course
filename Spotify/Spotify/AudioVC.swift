c7f6946485cf608862bdf009dfa0f68285b3d92d
Anuj Gupta <junagupta19@gmail.com>
2017-04-16 22:10:35 +0530

    Added Songs list which contains album art and song name and also added preview song which plays for 30 sec

---
+++ Spotify/Spotify/AudioVC.swift
@@ -0,0 +1,77 @@
+//
+//  AudioVC.swift
+//  Spotify
+//
+//  Created by Anuj Gupta on 16/04/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+
+class AudioVC: UIViewController {
+    
+    var image = UIImage()
+    var name = String()
+    var mainPreviewUrl = String()
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var songName: UILabel!
+    
+    override func viewDidLoad() {
+        
+        songName.text = name
+        mainImage.image = image
+        backgroundImage.image = image
+        
+        downloadFilePlay(url: URL(string: mainPreviewUrl)!)
+        
+    playPauseButton.setTitle("Pause", for: .normal)
+        
+    }
+    
+    func downloadFilePlay(url : URL){
+        
+        var downloadTask = URLSessionDownloadTask()
+        downloadTask = URLSession.shared.downloadTask(with: url, completionHandler: {
+            customURL,response,error in
+             self.play(url: customURL!)
+        })
+        
+        downloadTask.resume()
+        
+    }
+    
+    func play(url : URL){
+        
+       
+        do{
+            
+        try player = AVAudioPlayer(contentsOf: url)
+        player.prepareToPlay()
+        player.play()
+            
+        }catch{
+            print(error)
+        }
+        
+    }
+
+    @IBAction func pausPlay(_ sender: Any) {
+        
+        if player.isPlaying {
+            player.pause()
+            playPauseButton.setTitle("Play", for: .normal)
+        }else{
+                player.play()
+            playPauseButton.setTitle("Pausek", for: .normal)
+        }
+        
+    }
+    
+}
--- Spotify/Spotify/ViewController.swift
+++ Spotify/Spotify/ViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 import Alamofire
+import AVFoundation
+
+var player = AVAudioPlayer()
+
+struct post {
+    let image : UIImage!
+    let name : String!
+    let previewURL :String!
+}
 
 class ViewController: UITableViewController {
 
     var searchUrl = "https://api.spotify.com/v1/search?q=Shawn&type=track"
     typealias JSONStandard = [String : AnyObject]
-    var namesArray = [String]()
+    var posts = [post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,14 +50,27 @@ class ViewController: UITableViewController {
             var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
             print(readableJSON)
             if let tracks = readableJSON["tracks"] as? JSONStandard {
-                if let items = tracks["items"]{
+                if let items = tracks["items"] as? [JSONStandard]{
                     for i in 0..<items.count{
-                        let item = items[i] as! JSONStandard
+                        let item = items[i]
                         let name = item["name"] as! String
-                        namesArray.append(name)
+                        let preview_url = item["preview_url"] as! String
+                        
+                        if let album = item["album"]{
+                            if let images = album["images"] as? [JSONStandard] {
+                                let imageData = images[0]
+                                let imageUrl = URL(string : imageData["url"] as! String)
+                                let mainImageData = NSData(contentsOf: imageUrl!)
+                                
+                                let mainImage = UIImage(data : mainImageData as! Data)
+                                posts.append(post.init(image: mainImage, name: name,previewURL: preview_url))
+                                self.tableView.reloadData()
+
+                            }
+                        }
+                        
                     }
                     
-                    self.tableView.reloadData()
                     
                 }
             }
@@ -64,15 +86,35 @@ class ViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return namesArray.count
+        return posts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = namesArray[indexPath.row]
+        let myMainImageView = cell.viewWithTag(1) as! UIImageView
+        
+        myMainImageView.image = posts[indexPath.row].image
+        
+        let myMainLabel = cell.viewWithTag(2) as! UILabel
+        
+        myMainLabel.text = posts[indexPath.row].name
+        
+        
+        
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indexPath = self.tableView.indexPathForSelectedRow?.row
+        
+        let vc = segue.destination as! AudioVC
+        vc.image = posts[indexPath!].image
+        vc.name = posts[indexPath!].name
+        vc.mainPreviewUrl = posts[indexPath!].previewURL
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
