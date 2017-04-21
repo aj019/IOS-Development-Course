cb792aad12c6352d2016ac15755f09cfdf3ef66b
Anuj Gupta <junagupta19@gmail.com>
2017-04-21 17:52:52 +0530

    Added Score functionality

--- PingPong/PingPong/GameScene.swift
+++ PingPong/PingPong/GameScene.swift
@@ -15,12 +15,22 @@ class GameScene: SKScene {
     var main = SKSpriteNode()
     var enemy = SKSpriteNode()
     
+    var main_score = SKLabelNode()
+    var enemy_score = SKLabelNode()
+    
+    var score = [Int]()
+    
     
     override func didMove(to view: SKView) {
+        
+        startGame()
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         main = self.childNode(withName: "main") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        main_score = self.childNode(withName: "main_score") as! SKLabelNode
+        enemy_score = self.childNode(withName: "enemy_score") as! SKLabelNode
+        
         
         ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
         
@@ -58,5 +68,41 @@ class GameScene: SKScene {
         
         enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
         
+        if ball.position.y <= main.position.y - 50{
+            
+            addScore(playerWhoWon: enemy)
+            
+            
+        }else if ball.position.y >= enemy.position.y + 50 {
+            
+            addScore(playerWhoWon: main)
+        }
+        
+        main_score.text = "\(score[0])"
+        enemy_score.text = "\(score[1])"
+        
     }
+    
+    func startGame(){
+        score = [0,0]
+        
+        main_score.text = "\(score[0])"
+        enemy_score.text = "\(score[1])"
+    }
+    
+    func addScore(playerWhoWon : SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == main{
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }else{
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        }
+        
+    }
+    
 }
